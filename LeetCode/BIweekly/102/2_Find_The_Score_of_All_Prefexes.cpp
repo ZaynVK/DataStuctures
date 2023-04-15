bf32b36e2230efdadd3b12c2a990254854e4cf3d
ZaynVK <106418792+ZaynVK@users.noreply.github.com>
2023-04-15 22:49:07 +0530

    Update 2_Find_The_Score_of_All_Prefexes.cpp

--- LeetCode/BIweekly/102/2_Find_The_Score_of_All_Prefexes.cpp
+++ LeetCode/BIweekly/102/2_Find_The_Score_of_All_Prefexes.cpp
@@ -1,1 +1,20 @@
 
+
+
+
+
+code:
+vector<long long> findPrefixScore(vector<int>& nums) {
+        vector<long long> ans;
+        long long sum = 0;
+        int maxi = INT_MIN;
+        for(int x : nums) {
+          maxi = max(maxi, x);
+          sum += x + maxi;
+          ans.push_back(sum);
+        }
+      return ans;
+    }
+
+Tc: O(n);
+sc: O(n);
