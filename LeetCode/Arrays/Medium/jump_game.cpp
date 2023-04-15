13207ab4cbab5d72aa433a5cdfaf2ce13e77ac39
ZaynVK <106418792+ZaynVK@users.noreply.github.com>
2023-04-15 23:03:57 +0530

    Update jump_game.cpp

--- LeetCode/Arrays/Medium/jump_game.cpp
+++ LeetCode/Arrays/Medium/jump_game.cpp
@@ -1,7 +1,7 @@
 //qustion:-
 You are given an integer array nums. You are initially positioned at the
 array's first index, and each element in the array represents your maximum jump length at that position.
-Return true if you can reach the last index, or false otherwise.
+Return true if you can reach the last index, or false otherwise. 
   
 Example 1:
 Input: nums = [2,3,1,1,4]
